baa3ba393bf313fbdbcbafd95c11c41d41509804
Cem Dırman <cemdrman@gmail.com>
2017-04-20 01:26:28 +0300

    added login method

--- app/src/main/java/com/develivery/cem/deliveryservice/activity/LoginActivity.java
+++ app/src/main/java/com/develivery/cem/deliveryservice/activity/LoginActivity.java
@@ -2,15 +2,30 @@ package com.develivery.cem.deliveryservice.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.develivery.cem.deliveryservice.R;
+import com.develivery.cem.deliveryservice.database.TokenDB;
 import com.develivery.cem.deliveryservice.model.Staff;
 import com.develivery.cem.deliveryservice.request.Demand;
+import com.develivery.cem.deliveryservice.request.RequestURL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends Activity {
 
@@ -18,49 +33,95 @@ public class LoginActivity extends Activity {
     private EditText txtSifre;
     private Button btnGiris;
     private Demand demand;
-    private SharedPreferences preferences;
+    private TokenDB tokenDB;
+    private String token;
+    private int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        initial();
-
-        btnGiris.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = txtEmail.getText().toString();
-                String sifre = txtSifre.getText().toString();
-                Staff staff = new Staff();
-                staff.setEmail(email);
-                staff.setPassword(sifre);
-
-
-
-                /**
-                 * splah ekranda db boş mu? --> dolu olması token var demektir
-                 * db boş ise --> login ekranı --> bilgiler alınıp request atılır
-                 * db dolu ise --> order ekranı
-                 */
-
-
-                demand.sendRequestForLogin(staff);
-                //demand.getAllProducts(getApplicationContext());
-
-                Intent ıntent = new Intent(getApplicationContext(),OrderActivity.class);
-                ıntent.putExtra("staffID", 1);
-                startActivity(ıntent);
-            }
-        });
+        init();
+        if (tokenDB.getRowCount() > 0) {
+            System.out.println();
+            Intent ıntent = new Intent(LoginActivity.this,OrderActivity.class );
+            startActivity(ıntent);
+        }else{
+            btnGiris.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String email = txtEmail.getText().toString();
+                    String sifre = txtSifre.getText().toString();
+                    if (email.equals("") || sifre.equals("")) {
+                        Toast.makeText(getApplicationContext(), "Lütfen Bilgilerizi Tam Giriniz!", Toast.LENGTH_LONG).show();
+                    }else{
+                        Staff staff = new Staff();
+                        staff.setEmail(email);
+                        staff.setPassword(sifre);
+                        login(staff);
+                    }
+                }
+            });
+        }
     }
 
-    private void initial(){
+    private void login(final Staff staff){
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, RequestURL.baseUrl.concat(RequestURL.loginUrl),null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println("response:" + response);
+
+                try {
+                    token = response.getString("token");
+                    id = response.getInt("id");
+                    System.out.println("staff token:" + token);
+                    Intent ıntent = new Intent(getApplicationContext(),OrderActivity.class);
+                    ıntent.putExtra("staffID", id);
+                    startActivity(ıntent);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+                Toast.makeText(getApplicationContext(), "Geçersiz email yada şifre!", Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                return params;
+            }
+            @Override
+            public byte[] getBody() {
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.accumulate("email",staff.getEmail());
+                    jsonObject.accumulate("password", staff.getPassword());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                String string_json = jsonObject.toString();
+                System.out.println("request json body:" + string_json);
+                return string_json.getBytes();
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    private void init(){
         txtEmail = (EditText) findViewById(R.id.email);
         txtSifre = (EditText) findViewById(R.id.password);
         btnGiris = (Button) findViewById(R.id.giris);
 
         //----
         demand = new Demand(getApplicationContext());
-        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        tokenDB = new TokenDB(getApplicationContext());
     }
 
 }
