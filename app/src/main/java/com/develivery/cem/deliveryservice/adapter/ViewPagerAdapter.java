79839a944d7fc43c3cea3d9bae51eabb8d26f259
Cem Dırman <cemdrman@gmail.com>
2017-04-23 10:49:10 +0300

    fragment name changed

--- app/src/main/java/com/develivery/cem/deliveryservice/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/develivery/cem/deliveryservice/adapter/ViewPagerAdapter.java
@@ -3,9 +3,8 @@ package com.develivery.cem.deliveryservice.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-
 import com.develivery.cem.deliveryservice.activity.ApprovedFragment;
-import com.develivery.cem.deliveryservice.activity.OnWayFragment;
+import com.develivery.cem.deliveryservice.activity.TakenOffFragment;
 import com.develivery.cem.deliveryservice.activity.DeliveredFragment;
 
 /**
@@ -22,7 +21,7 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
         if (position ==0) {
             return new ApprovedFragment();
         } else if (position == 1) {
-            return new OnWayFragment();
+            return new TakenOffFragment();
         } else return new DeliveredFragment();
     }
 
